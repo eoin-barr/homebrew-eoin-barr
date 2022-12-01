843e13d98197e89589c7005d7ef70ba2a4ec47fc
eoin-barr <eoin.barr@ucdconnect.ie>
2022-12-01 18:56:47 +0000

    Brew formula update for weatherme version v1.7

--- weatherme.rb
+++ weatherme.rb
@@ -5,11 +5,11 @@
 class Weatherme < Formula
   desc ""
   homepage "https://github.com/eoin-barr/weatherme"
-  version "1.6"
+  version "1.7"
 
   on_macos do
-    url "https://github.com/eoin-barr/weatherme/releases/download/v1.6/weatherme_1.6_darwin_all.tar.gz"
-    sha256 "6ca9f9c872bc8687dff4d9f3545b274670218cd86026ff183a7b6e32742e43ee"
+    url "https://github.com/eoin-barr/weatherme/releases/download/v1.7/weatherme_1.7_darwin_all.tar.gz"
+    sha256 "8e59cef9a24707ba19d8308dd938b6d31609f0f1939f0e5a878d5eb49131f182"
 
     def install
       bin.install "weatherme"
@@ -18,16 +18,16 @@ class Weatherme < Formula
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/eoin-barr/weatherme/releases/download/v1.6/weatherme_1.6_linux_arm64.tar.gz"
-      sha256 "c6e537b566198274d5cc02d2805511d4b1a899de8fd34b9cfedd886684f11ab1"
+      url "https://github.com/eoin-barr/weatherme/releases/download/v1.7/weatherme_1.7_linux_arm64.tar.gz"
+      sha256 "922ad04966e3832907ff72a02347a5306366904045422cc31f30132d1b8a34c4"
 
       def install
         bin.install "weatherme"
       end
     end
     if Hardware::CPU.intel?
-      url "https://github.com/eoin-barr/weatherme/releases/download/v1.6/weatherme_1.6_linux_amd64.tar.gz"
-      sha256 "76a98f3311f88b5124531051a979611de728958634d4f2685bd9913e4cdbe662"
+      url "https://github.com/eoin-barr/weatherme/releases/download/v1.7/weatherme_1.7_linux_amd64.tar.gz"
+      sha256 "cdcf89564e2140a990b175d12a48614e633eeced4310ecc3ba89601db6687b3a"
 
       def install
         bin.install "weatherme"
